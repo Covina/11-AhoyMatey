5bd7ae80866bb80224dc61ab20c872d7f96d57b4
Covina <jabs303@gmail.com>
2017-03-19 16:07:52 -0700

    Updated with skybox assets.

--- Assets/Players/Player.cs
+++ Assets/Players/Player.cs
@@ -7,9 +7,15 @@ public class Player : NetworkBehaviour {
 
 	private Vector3 inputValue;
 
+	private Animator animator;
+
+
 	// Use this for initialization
 	void Start () {
-	
+
+		animator = GetComponent<Animator>();
+
+
 	}
 	
 	// Update is called once per frame
@@ -28,6 +34,16 @@ public class Player : NetworkBehaviour {
 		inputValue.z = CrossPlatformInputManager.GetAxis ("Vertical");
 
 		transform.Translate (inputValue);
+
+		Debug.Log ("Position of Player: " + transform.position + " ||  Rotation of Player: " + transform.rotation);
+
+	}
+
+	// Override to add new functionality, here we are activating the camera. 
+	public override void OnStartLocalPlayer() 
+	{
+		// Activate the camera for the local player
+		GetComponentInChildren<Camera>().enabled = true;
 
 
 
