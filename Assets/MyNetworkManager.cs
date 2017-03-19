1c9332f4dfe3bba37aec31383e9d082c556abd9a
Covina <jabs303@gmail.com>
2017-03-19 11:41:52 -0700

    Initial setup and basic network manager

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+	public void MyStartHost ()
+	{
+		Debug.Log("Starting Host at: " + Time.timeSinceLevelLoad);
+		StartHost();
+
+	}
+
+
+	public override void OnStartHost()
+	{
+		Debug.Log("Host Started at: " + Time.timeSinceLevelLoad);
+
+	}
+
+
+}
