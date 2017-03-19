eaf4f8002f004219a5d9ffcfbe5efb75f911a2fb
Covina <jabs303@gmail.com>
2017-03-19 13:24:51 -0700

    Now working with PS4 controller.

---
+++ Assets/Players/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 inputValue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		inputValue.x = CrossPlatformInputManager.GetAxis("Horizontal");
+		inputValue.y = 0f;
+		inputValue.z = CrossPlatformInputManager.GetAxis("Vertical");
+
+		transform.Translate(inputValue);
+
+
+	}
+
+
+
+
+
+}
